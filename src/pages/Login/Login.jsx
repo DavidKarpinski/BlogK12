fb820ff71f84904f12a9472663b381539e8c984a
DavidKarpinski <karpinski.david@tuta.io>
2024-04-02 16:47:13 -0300

    feat: login form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,19 @@
+import { Link } from 'react-router-dom'
+
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div id='login'>
+      <h1 className='form-h1'>Login</h1>
+      <h2 className='form-h2'>Novo no site? <Link to='/register'>Registre-se</Link></h2>
+      <form id='login-form'>
+        <div>
+          <label for='email'>Email:</label>
+          <input type='email' name='email' placeholder='Email...'></input>
+          <label for='password'>Senha:</label>
+          <input type='password' name='password' placeholder='Senha...'></input>
+          <input type='submit' id='submit' value='Login'></input>
+        </div>
+      </form>
     </div>
   )
 }
