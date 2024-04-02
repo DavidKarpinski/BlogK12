7617ed794cc91cf41a254574c9cfeb0b52984ac8
DavidKarpinski <karpinski.david@tuta.io>
2024-04-02 15:19:45 -0300

    feat: navbar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+function NavBar() {
+  return (
+    <nav className='navbar'>
+      <p>Blog K-12</p>
+      <div className="navbar__buttons">
+        <button>Posts</button>
+        <button>Login</button>
+        <button>Registre-se</button>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
