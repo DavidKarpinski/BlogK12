0a6b797394262c5cdc1a3906376c35b24cad3425
DavidKarpinski <karpinski.david@tuta.io>
2024-04-02 16:48:27 -0300

    style: login and register forms

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
     <nav className='navbar'>
       <div className='nav-content'>
         <p className='navbar__title'>Blog K-12</p>
-        <div className='navbar__buttons'>
+        <div>
           <a href='/'><button id='posts'>Posts</button></a>
           <a href='/login'><button id='login'>Login</button></a>
           <a href='/register'><button id='register'>Registre-se</button></a>
