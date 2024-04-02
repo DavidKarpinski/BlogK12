45c78c3ee66cdb535da983ae33089d391d358060
DavidKarpinski <karpinski.david@tuta.io>
2024-04-02 16:47:37 -0300

    feat: register form

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,21 @@
+import { Link } from 'react-router-dom'
+
 function Register() {
   return (
-    <div>
-      <h1>Register</h1>
+    <div id='register'>
+      <h1 className='form-h1'>Registre-se</h1>
+      <h2 className='form-h2'>Já tem uma conta? <Link to='/login'>Faça login</Link></h2>
+      <form id='register-form'>
+        <div>
+          <label for='name'>Nome:</label>
+          <input type='text' name='name' placeholder='Nome...' />
+          <label for='email'>Email:</label>
+          <input type='email' name='email' placeholder='Email...'></input>
+          <label for='password'>Senha:</label>
+          <input type='password' name='password' placeholder='Senha...'></input>
+          <input type='submit' id='submit' value='Registrar'></input>
+        </div>
+      </form>
     </div>
   )
 }
