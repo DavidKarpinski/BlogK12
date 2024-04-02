5525807232c26ed0644d740969c5527d33691262
DavidKarpinski <karpinski.david@tuta.io>
2024-04-02 15:20:19 -0300

    feat: footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Footer from './components/Footer/Footer'
+import NavBar from './components/NavBar/NavBar'
 import Router from './components/Router'
 
 function App() {
   return (
     <div className='App'>
+      <NavBar />
       <Router />
+      <Footer />
     </div>
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,15 @@
+import './NavBar.css'
+
 function NavBar() {
   return (
     <nav className='navbar'>
-      <p>Blog K-12</p>
-      <div className="navbar__buttons">
-        <button>Posts</button>
-        <button>Login</button>
-        <button>Registre-se</button>
+      <div className='nav-content'>
+        <p className='navbar__title'>Blog K-12</p>
+        <div className='navbar__buttons'>
+          <a href='/'><button id='posts'>Posts</button></a>
+          <a href='/login'><button id='login'>Login</button></a>
+          <a href='/register'><button id='register'>Registre-se</button></a>
+        </div>
       </div>
     </nav>
   )
