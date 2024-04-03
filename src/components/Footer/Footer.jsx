00fc74fcef1dc09bf90adcd131276ce4132a9dd9
DavidKarpinski <karpinski.david@tuta.io>
2024-04-03 19:22:54 -0300

    fix: footer copyright symbol render

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,9 @@
 import './Footer.css'
 
 function Footer() {
-  const footerText = '&copy; 2024 XGROW K-12, Todos os direitos reservados'
   return (
     <footer className='footer'>
-      <p className='footer__text'>{footerText}</p>
+      <p className='footer__text'>&copy; 2024 XGROW K-12, Todos os direitos reservados</p>
     </footer>
   )
 }
